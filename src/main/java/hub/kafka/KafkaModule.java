ea19cbd43386bc1c8fd9c6c76ebd8af93057d619
tbirnbaum <tbirnbaum@team.mobile.de>
2016-12-20 17:33:24 +0100

    some kafka improvements

--- src/main/java/hub/kafka/KafkaModule.java
+++ src/main/java/hub/kafka/KafkaModule.java
@@ -6,6 +6,9 @@ import dagger.Module;
 import dagger.Provides;
 
 import hub.config.Config;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import java.util.Properties;
 
 @Module
 public class KafkaModule {
@@ -15,4 +18,19 @@ public class KafkaModule {
         return new TopicConnectionFactory(Config.mustExist("kafka.databroker.host"));
     }
 
+    @Provides @Singleton
+    KafkaProducer producer() {
+        Properties props = new Properties();
+        props.put("client.id", "dealership-profile-service");
+        props.put("bootstrap.servers", Config.mustExist("kafka.databroker.host"));
+        props.put("acks", "all");
+        props.put("retries", java.lang.Integer.valueOf(32));
+        props.put("compression.type", "lz4");
+
+        return new KafkaProducer(
+                props,
+                new org.apache.kafka.common.serialization.StringSerializer(),
+                new org.apache.kafka.common.serialization.StringSerializer()
+        );
+    }
 }
--- src/main/java/hub/kafka/Publisher.java
+++ src/main/java/hub/kafka/Publisher.java
@@ -1,4 +1,33 @@
 package hub.kafka;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.lang.reflect.Type;
+import java.util.Map;
+
 public class Publisher {
+
+    private final KafkaProducer producer;
+    private static final Type TYPE = new TypeToken<Map<String, Object>>(){}.getType();
+    private final Gson gson;
+
+    public Publisher(KafkaProducer producer, Gson gson){
+        this.producer = producer;
+        this.gson = gson;
+    }
+
+    public void changed(Map<String, Object> map) {
+
+        ProducerRecord record = new ProducerRecord(
+                "hub_event",
+                (String)map.get("id"),
+                gson.toJson(map, TYPE)
+        );
+
+        producer.send(record);
+    }
 }
